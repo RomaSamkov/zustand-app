a5348a13299f218fac7eca912c3a51650e3ff44a
RomaSamkov <Romansamkov.rs@gmail.com>
2024-11-04 20:14:39 +0200

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -2,6 +2,16 @@ import { create } from "zustand";
 
 type CounterStore = {
   count: number;
+  increment: () => void;
+  deccrement: () => void;
 };
 
-export const useCounterStore = create<CounterStore>(() => ({ count: 0 }));
+export const useCounterStore = create<CounterStore>((set) => ({
+  count: 0,
+  increment() {
+    set({ count: 1 });
+  },
+  deccrement() {
+    set({ count: -1 });
+  },
+}));
