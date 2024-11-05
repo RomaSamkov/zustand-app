1a1b2be6d7f97818bcca9b18053247a100a6a25f
RomaSamkov <Romansamkov.rs@gmail.com>
2024-11-05 16:48:13 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
+import { useEffect } from "react";
 import "./App.css";
 import { useCounterStore } from "./store";
 
-function App() {
+const logCount = () => {
+  const count = useCounterStore.getState().count;
+  console.log("🚀 ~ logCount ~ count:", count);
+};
+
+const SetCount = () => {
+  useCounterStore.setState({ count: 1 });
+};
+
+const App = () => {
   const count = useCounterStore((state) => state.count);
   return <OtherComponent count={count} />;
-}
+};
 
 export const OtherComponent = ({ count }: { count: number }) => {
   const increment = useCounterStore((state) => state.increment);
+  const incrementAsync = useCounterStore((state) => state.incrementAsync);
   const decrement = useCounterStore((state) => state.deccrement);
+
+  useEffect(() => {
+    logCount();
+    SetCount();
+  }, []);
+
   return (
     <div>
       <h2>{count}</h2>
       <div className="buttons">
         <button onClick={increment}>Increment</button>
+        <button onClick={incrementAsync}>IncrementAsync</button>
 
         <button onClick={decrement}>Decrement</button>
       </div>
